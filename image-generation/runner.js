9dc3c583733952fc0a9b5be4531c42bbd5747ccc
Daniel D. Beck <daniel.david.beck@gmail.com>
2019-07-06 18:14:27 +0100

    Write images to dedicated folder

--- image-generation/runner.js
+++ image-generation/runner.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const puppeteer = require('puppeteer');
 const handler = require('serve-handler');
 const http = require('http');
@@ -23,7 +25,7 @@ async function grab() {
   await page.goto('http://localhost:3000/');
 
   await (await page.$('#eink-content')).screenshot({ path: 'element.png' })
-  await page.screenshot({ path: 'example.png' });
+  await page.screenshot({ path: 'output/eink.png' });
 
   console.log('Stopping puppeteer');
   await browser.close();
