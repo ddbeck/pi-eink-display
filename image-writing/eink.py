60f05a154113cd644bc857a7adb111b918b2ce99
Daniel D. Beck <daniel.david.beck@gmail.com>
2019-07-07 18:40:23 +0100

    Fix eink writing

--- image-writing/eink.py
+++ image-writing/eink.py
@@ -3,7 +3,15 @@
 from PIL import Image
 from inky import InkyWHAT
 
-inkywhat = InkyWHAT("red")
-
+# get image from disk
 im = Image.open("../image-generation/output/eink.png")
-inkywhat.set_image(im)
+
+# convert to black, white, and red
+palette = Image.new("P", (1, 1))
+palette.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
+bwr_image = im.convert("RGB").quantize(palette=palette)
+
+# write to eink display
+inkywhat = InkyWHAT("red")
+inkywhat.set_image(bwr_image)
+inkywhat.show()
